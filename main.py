26e6fc9fc1ca9bbf95041ccd4ecc9b50aced383a
Мария Окишева <mashok.mo@gmail.com>
2022-11-26 19:27:56 +0300

    optimize code

--- main.py
+++ main.py
@@ -1,29 +1,18 @@
-#Пять типов студентов: ботаник, спортсмен, чиллер, роцкер
-#БОТАНИК
-#75% - study, 5% - do sport, 10% - fun, 10% - relax
-#СПОРТСМЕН
-#15% - study, 65% - do sport, 5% - fun, 15% - relax
-#ЧИЛЛЕР
-#20% - study, 0% - do sport, 10% - fun, 70% - relax
-#РОЦКЕР
-#15% - study, 10% - do sport, 65% - fun, 10% - relax
-
 import random
 
 class Student:
-    def __init__(self, health, energy, progress, happy, intelligence, predisposition):
+    def __init__(self, health, energy, progress, happy, predisposition):
         self.health = health
         self.energy = energy
         self.progress = progress
         self.happy = happy
-        self.intelligence = intelligence
         self.predisposition = predisposition
 
     def study(self):
-        self.energy -= random.randint(5,10)
-        self.health -= random.randint(1,5)
-        self.progress += random.randint(10,20)
-        self.happy -= random.randint(5,15)
+        self.energy -= random.randint(10,20)
+        self.health -= random.randint(10,20)
+        self.progress += random.randint(20,25)
+        self.happy -= random.randint(10,20)
         return('учился')
     def relax(self):
         self.happy += random.randint(5,10)
@@ -32,70 +21,44 @@ class Student:
         return('отдыхал')
     def fun(self):
         self.energy += random.randint(5,10)
-        self.health -= random.randint(1,5)
-        self.happy += random.randint(5,15)
+        self.health -= random.randint(10,20)
+        self.happy += random.randint(5,10)
         return('развлекался')
     def do_sport(self):
-        self.energy -= random.randint(5,15)
+        self.energy -= random.randint(15,20)
         self.health += random.randint(5,10)
         self.happy += random.randint(5,10)
         return('занимался спортом')
 
-    #def f(self):
+    def check_property(self, property):
+        if property <= 0:
+            return(False)
 
+    def action_definition(self, seed, n1, n2, n3, study, do_sport, fun, relax):
+        if seed <= n1:
+            return(study)
+        elif seed > n1 and seed <= n2:
+            return (do_sport)
+        elif seed > n2 and seed <= n3:
+            return (fun)
+        else:
+            return (relax)
 
     def update(self):
         seed = random.randint(0,100)
         if self.predisposition == 1: #ботаник
-            if seed <=75:
-                return (self.study())
-            elif seed >75 and seed <=80:
-                return (self.do_sport())
-            elif seed >80 and seed <=90:
-                return (self.fun())
-            elif seed >90:
-                return (self.relax())
+            return(self.action_definition(seed, 75, 80, 90, self.study(), self.do_sport(), self.fun(), self.relax()))
         elif self.predisposition == 2: #спортсмен
-            if seed <=15:
-                return (self.study())
-            elif seed >15 and seed <=80:
-                return (self.do_sport())
-            elif seed >80 and seed <=85:
-                return (self.fun())
-            elif seed >85:
-                return (self.relax())
+            return (self.action_definition(seed, 15, 80, 85, self.study(), self.do_sport(), self.fun(), self.relax()))
         elif self.predisposition == 3: #чиллер
-            if seed <= 20:
-                return (self.study())
-            elif seed > 20 and seed <= 30:
-                return (self.fun())
-            elif seed > 30:
-                return (self.relax())
+            return (self.action_definition(seed, 20, 20, 30, self.study(), self.do_sport(), self.fun(), self.relax()))
         elif self.predisposition == 4: #роцкер
-            if seed <=15:
-                return (self.study())
-            elif seed >15 and seed <=25:
-                return (self.do_sport())
-            elif seed >25 and seed <=90:
-                return (self.fun())
-            elif seed >90:
-                return (self.relax())
-
-    def check_health(self):
-        if self.health <=0:
-            return(False)
-
-    def check_happy(self):
-        if self.happy <=0:
-            return(False)
+            return (self.action_definition(seed, 15, 25, 90, self.study(), self.do_sport(), self.fun(), self.relax()))
 
     def check_progress(self):
         if self.progress <50:
             return(False)
 
-    def check_energy(self):
-        if self.energy <=0:
-            return(False)
 
 class Controller:
     def __init__(self, num_student):
@@ -103,48 +66,76 @@ class Controller:
         self.student_list = []
 
     def create_student(self):
+        print_list = []
         for i in range(self.num_student):
-            student = Student(100,100,0,100,0,random.randint(1,4))
+            student = Student(80,80,0,80,random.randint(1,4))
             if student.predisposition == 1:
-                student.intelligence = 80
+                print_list.append(f'Cтудент {i+1} - ботаник')
             elif student.predisposition == 2:
-                student.intelligence = 30
+                print_list.append(f'Cтудент {i+1} - спортсмен')
             elif student.predisposition == 3:
-                student.intelligence = 50
+                print_list.append(f'Cтудент {i+1} - чиллер')
             elif student.predisposition == 4:
-                student.intelligence = 40
+                print_list.append(f'Cтудент {i+1} - роцкер')
             self.student_list.append(student)
+        return (print_list)
 
     def update_semester(self):
+        for j in range(len(self.student_list)):
+            self.student_list[j].health = 80
+            self.student_list[j].energy = 80
+            self.student_list[j].happy = 80
+            self.student_list[j].progress = 0
         print_list = []
         delete_student_list = []
-        for j in range(self.num_student):
-            print_list.append(f'Студент {j+1} весь месяц {self.student_list[j].update()}.\nЗдоровье: {self.student_list[j].health}\nЭнергия: {self.student_list[j].energy}\nСчастье: {self.student_list[j].happy}\nУспеваемость: {self.student_list[j].progress}\nИнтеллект: {self.student_list[j].intelligence}')
 
-            if self.student_list[j].check_health() == False:
-                print_list.append(f'Сердце студента {j+1} не выдержало этот семестр')
+        def update_month(self):
+            print_list.append(f'\nМесяц {i+1}')
+            for j in range(len(self.student_list)):
+                print_list.append(f'\nСтудент {j+1} весь месяц {self.student_list[j].update()}.\nЗдоровье: {self.student_list[j].health}\nЭнергия: {self.student_list[j].energy}\nСчастье: {self.student_list[j].happy}\nУспеваемость: {self.student_list[j].progress}')
+
+        for i in range(0, 5):
+            update_month(self)
+
+        print_list.append(f'\nРезультаты семестра: ')
+
+        for j in range(len(self.student_list)):
+            if self.student_list[j].check_property(self.student_list[j].health) == False:
+                print_list.append(f'Сердце студента {j + 1} не выдержало этот семестр')
                 delete_student_list.append(self.student_list[j])
 
-            elif self.student_list[j].check_energy() == False:
-                print_list.append(f'Студент {j+1} упал без сил перед стенами университета')
+            elif self.student_list[j].check_property(self.student_list[j].energy) == False:
+                print_list.append(f'Студент {j + 1} упал без сил перед стенами университета')
                 delete_student_list.append(self.student_list[j])
 
-            elif self.student_list[j].check_happy() == False:
-                print_list.append(f'Студент {j+1} попал в психбольницу с тяжелой депрессией')
+            elif self.student_list[j].check_property(self.student_list[j].happy) == False:
+                print_list.append(f'Студент {j + 1} попал в психбольницу с тяжелой депрессией')
                 delete_student_list.append(self.student_list[j])
 
             elif self.student_list[j].check_progress() == False:
-                print_list.append(f'Студент {j+1} не закрыл фх')
+                print_list.append(f'Студент {j + 1} не закрыл фх')
                 delete_student_list.append(self.student_list[j])
 
+            else:
+                print_list.append(f'Поздравим студента {j + 1}! Он закрыл семестр')
+
+        if len(delete_student_list)!=0:
+            for i in range(len(delete_student_list)):
+                self.student_list.remove(delete_student_list[i])
+
         return(print_list)
+
+def print_list(list):
+    for i in range(len(list)):
+        print(list[i])
 
 def main():
     num_student = int(input('Введите количество студентов: '))
     num_semester = int(input('Введите количество семестров: '))
     controller = Controller(num_student)
-    controller.create_student()
+    print_list(controller.create_student())
     for i in range (num_semester):
-        print(controller.update_semester())
+        print(f'\nСеместр {i+1}')
+        print_list(controller.update_semester())
 main()
 
